0f79077e82ac3f5d6bd43954c515be792a96fbbc
FatalSleep <fatalsleep@gmail.com>
2016-03-21 17:23:37 -0700

    Create eventcaller.cpp

---
+++ Examples/eventcaller.cpp
@@ -0,0 +1,29 @@
+/*
+  Simple example that creates an event caller and listener. A function
+  from the listener is subscribed to the caller, then the caller invokes
+  the event. When the event is invoked the listener is notified.
+*/
+#include <iostream>
+#include "invokable.hpp"
+
+class EventCaller {
+public:
+    invokable<> button_press;
+};
+
+class EventListener {
+public:
+    void notifier() {
+        cout << "Button Pressed" << endl;
+    };
+};
+
+int main() {
+    EventCaller caller;
+    EventListener listener;
+    
+    callback<> call = caller.button_press.create(&listener, &EventListener::notifier);
+    caller += call;
+    caller();
+    caller -= call;
+};
