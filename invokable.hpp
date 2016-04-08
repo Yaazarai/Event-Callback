3a75597e4b01be26920982d460697f301389ae2b
Alexander Griffis <fatalsleep@gmail.com>
2016-04-08 09:41:47 -0700

    Minor changes and addition of const on operators.
    - Removed C-Style casts.
    - Added constexpr to the hash_code() function. The hash can be determined at compile time., so this is valid.
    - Added const to operators since the returned value of the operators is always constant.

--- invokable.hpp
+++ invokable.hpp
@@ -32,42 +32,42 @@
     template<typename... A>
     class callback {
     private:
-        size_t hash = 0;
+        size_t hash;
         std::function<void(A...)> func;
 
         template<typename T, class _Fx, int... Is>
         void create(T* obj, _Fx&& func, int_sequence<Is...>) {
-            hash = (size_t)&this->func ^ (&typeid(callback<A...>))->hash_code();
             this->func = std::function<void(A...)>(std::bind(func, obj, placeholder_template<Is>{}...));
         };
 
         template<class _Fx, int... Is>
         void create(_Fx&& func, int_sequence<Is...>) {
             this->func = std::function<void(A...)>(std::bind(func, placeholder_template<Is>{}...));
-            hash = (size_t)&this->func ^ (&typeid(callback<A...>))->hash_code();
         };
 
     public:
-        inline bool operator == (const callback& cb) {
+        const inline bool operator == (const callback& cb) {
             return (hash == cb.hash);
         };
 
-        inline bool operator != (const callback& cb) {
+        const inline bool operator != (const callback& cb) {
             return (hash != cb.hash);
         };
 
-        inline callback& operator () (A... args) {
+        const inline callback& operator () (A... args) {
             func(args...);
             return (*this);
         };
 
         template<typename T, class _Fx>
         callback(T* obj, _Fx&& func) {
+            hash = reinterpret_cast<size_t>(&this->func) ^ (&typeid(callback<A...>))->hash_code();
             create(obj, func, make_int_sequence<sizeof...(A)> {});
         };
 
         template<class _Fx>
         callback(_Fx&& func) {
+            hash = reinterpret_cast<size_t>(&this->func) ^ (&typeid(callback<A...>))->hash_code();
             create(func, make_int_sequence<sizeof...(A)> {});
         };
 
@@ -75,7 +75,7 @@
             func(args...);
         };
 
-        size_t hash_code() const {
+        constexpr size_t hash_code() const throw() {
             return hash;
         };
     };
@@ -90,28 +90,28 @@
         std::vector<callback<A...>> callbacks;
 
     public:
-        inline invokable& operator += (const callback<A...>& cb) {
+        const inline invokable& operator += (const callback<A...>& cb) {
             if (std::find(callbacks.begin(), callbacks.end(), cb) == callbacks.end())
                 callbacks.push_back(cb);
             return (*this);
         };
 
-        inline invokable& operator -= (const callback<A...>& cb) {
+        const inline invokable& operator -= (const callback<A...>& cb) {
             std::vector<callback<A...>>::iterator it;
             it = std::find(callbacks.begin(), callbacks.end(), cb);
-            
+
             if (it != callbacks.end())
                 callbacks.erase(it);
             return (*this);
         };
 
-        inline invokable& operator = (const callback<A...>& cb) {
+        const inline invokable& operator = (const callback<A...>& cb) {
             callbacks.clear();
             (*this) += cb;
             return (*this);
         };
 
-        inline invokable& operator () (A... args) {
+        const inline invokable& operator () (A... args) {
             invoke(args...);
             return (*this);
         };
