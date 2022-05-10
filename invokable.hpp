c1ee52a4a5de16657e76447ddd475b58caf46dfd
Alexander Griffis <fatalsleep@gmail.com>
2022-05-10 13:09:59 -0700

    Added null check to unhook all, added comments.

--- invokable.hpp
+++ invokable.hpp
@@ -7,8 +7,8 @@
     #include <algorithm>
     #include <mutex>
 
+    /// This generates the expansion for vargs into the invokable templated types.
     #pragma region Placeholder Generator
-
     template<int>
     struct placeholder_template {};
 
@@ -16,93 +16,94 @@
         template<int N>
         struct is_placeholder<placeholder_template<N>> : integral_constant<int, N + 1> {};
     };
-
     #pragma endregion
 
     #pragma region Invokable Callback
-
+    /// Function binder for dynamic callbacks.
     template<typename... A>
     class callback {
     private:
+        // Unique hash ID.
         size_t hash;
+        // Bound function.
         std::function<void(A...)> func;
-
+        
+        // Creates and binds the function callback with a class instance for member functions.
         template<typename T, class _Fx, std::size_t... Is>
         void create(T* obj, _Fx&& func, std::integer_sequence<std::size_t, Is...>) {
             this->func = std::function<void(A...)>(std::bind(func, obj, placeholder_template<Is>()...));
         };
 
+        // Creates and binds the function callback for a static/lambda/global function.
         template<class _Fx, std::size_t... Is>
         void create(_Fx&& func, std::integer_sequence<std::size_t, Is...>) {
             this->func = std::function<void(A...)>(std::bind(func, placeholder_template<Is>()...));
         };
 
     public:
-        const inline bool operator == (const callback& cb) {
-            return (hash == cb.hash);
-        };
-
-        const inline bool operator != (const callback& cb) {
-            return (hash != cb.hash);
-        };
-
-        const inline callback& operator () (A... args) {
-            func(args...);
-            return (*this);
-        };
-
+        /// Compares equality of callbacks.
+        const inline bool operator == (const callback& cb) { return (hash == cb.hash); };
+        
+        /// Compares not-equality of callbacks.
+        const inline bool operator != (const callback& cb) { return (hash != cb.hash); };
+        
+        /// Executes this callback with arguments.
+        const inline callback& operator () (A... args) { func(args...); return (*this); };
+        
+        /// Construct a callback with a template T reference for instance/member functions.
         template<typename T, class _Fx>
         callback(T* obj, _Fx&& func) {
             hash = reinterpret_cast<size_t>(&this->func) ^ (&typeid(callback<A...>))->hash_code();
             create(obj, func, std::make_integer_sequence<std::size_t, sizeof...(A)> {});
         };
 
+        /// Construct a callback with template _Fx for static/lambda/global functions.
         template<class _Fx>
         callback(_Fx&& func) {
             hash = reinterpret_cast<size_t>(&this->func) ^ (&typeid(callback<A...>))->hash_code();
             create(func, std::make_integer_sequence<std::size_t, sizeof...(A)> {});
         };
+        
+        /// Executes this callback with arguments.
+        callback& invoke(A... args) { func(args...); return (*this); };
 
-        void invoke(A... args) {
-            func(args...);
-        };
-
+        /// Returns this callbacks hash code.
         constexpr size_t hash_code() const throw() {
             return hash;
         };
     };
-
     #pragma endregion
 
     #pragma region Invokable Event
-
+    /// Thread-safe event handler for callbacks.
     template<typename... A>
     class invokable {
     private:
+        /// Shared resource to manage multi-thread resource locking.
         std::mutex safety_lock;
+        /// Vector list of function callbacks associated with this invokable event.
         std::vector<callback<A...>> callbacks;
 
     public:
+        /// Adds a callback to this event.
         const inline invokable& operator += (const callback<A...>& cb) {
-            hook(cb);
+            if (cb != nullptr) hook(cb);
             return (*this);
         };
-
-        const inline invokable& operator -= (const callback<A...>& cb) {
-            unhook();
-            return (*this);
-        };
-
+        
+        /// Removes a callback from this event.
+        const inline invokable& operator -= (const callback<A...>& cb) { unhook(cb); return (*this); };
+        
+        /// Removes all registered callbacks and adds a new callback.
         const inline invokable& operator = (const callback<A...>& cb) {
-            hook_unhook(cb);
+            if (cb != nullptr) { hook_unhook(cb); } else { unhook_all(); }
             return (*this);
         };
-
-        const inline invokable& operator () (A... args) {
-            invoke(args...);
-            return (*this);
-        };
-
+        
+        /// Execute all registered callbacks.
+        const inline invokable& operator () (A... args) { invoke(args...); return (*this); };
+        
+        /// Adds a callback to this event, operator +=
         invokable& hook(callback<A...> cb) {
             std::lock_guard<std::mutex> g(safety_lock);
 
@@ -111,7 +112,8 @@
 
             return (*this);
         };
-
+        
+        /// Removes a callback from this event, operator -=
         invokable& unhook(callback<A...> cb) {
             std::lock_guard<std::mutex> g(safety_lock);
 
@@ -123,7 +125,8 @@
 
             return (*this);
         };
-
+        
+        /// Removes all registered callbacks and adds a new callback, operator =
         invokable& hook_unhook(callback<A...> cb) {
             std::lock_guard<std::mutex> g(safety_lock);
 
@@ -132,7 +135,8 @@
 
             return (*this);
         };
-
+        
+        /// Removes all registered callbacks.
         invokable& unhook_all() {
             std::lock_guard<std::mutex> g(safety_lock);
 
@@ -140,7 +144,8 @@
 
             return (*this);
         };
-
+        
+        /// Execute all registered callbacks, operator ()
         invokable& invoke(A... args) {
             std::lock_guard<std::mutex> g(safety_lock);
 
