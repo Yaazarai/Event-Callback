a69a444e7360b7c9e59d816011558b0314430689
Alexander Griffis <fatalsleep@gmail.com>
2023-03-23 02:45:14 -0700

    Rewrite for RAII and Compiler compliance.
    - Removed placeholder template expansion for arguments.
    - Is now RAII compliant and more stable for the compiler.
    - Stack-Overflow Warnings and lack of compiler errors due to placeholder template expansion has been solved.
    - Hook/Unhook/Rehook/Invoke operators removed for explicit clarity on API call.
    - callback<A..> invoke call now allows for method chaining.

--- invokable.hpp
+++ invokable.hpp
@@ -1,102 +1,59 @@
 /// Source: https://stackoverflow.com/questions/9568150/what-is-a-c-delegate/9568485#9568485
-/// Source: https://stackoverflow.com/questions/21663012/binding-a-generic-member-function/21664270#21664270
+/// Source: https://en.cppreference.com/w/cpp/utility/functional/invoke
 #pragma once
 #ifndef INVOKABLE_CALLBACKS
 #define INVOKABLE_CALLBACKS
     #include <functional>
     #include <vector>
     #include <mutex>
-    
-    #pragma region Placeholder Generator
-    /// This generates the expansion for vargs into the invokable templated types.
-    template<int>
-    struct placeholder_template {};
+    #include <utility>
 
-    namespace std {
-        template<int N>
-        struct std::is_placeholder<placeholder_template<N>> : std::integral_constant<int, N + 1> {};
-    }
-    #pragma endregion
-    #pragma region Invokable Callback
-    /// Function binder for dynamic callbacks.
     template<typename... A>
     class callback {
     protected:
-        // Unique hash ID for class comparisons.
+        /// Unique identifying hash code.
         size_t hash;
-        // The function this callback represents.
-        std::function<void(A...)> func;
-
-        // Creates and binds the function callback with a class instance for member functions.
-        template<typename T, class _Fx, std::size_t... Is>
-        void create(T* obj, _Fx&& func, std::integer_sequence<std::size_t, Is...>) {
-            this->func = std::function<void(A...)>(std::bind(func, obj, placeholder_template<Is>()...));
+        /// The function bound to this callback.
+        std::function<void(A...)> bound;
+        
+        /// Binds a static or lambda function.
+        template<class Fx>
+        void bind_callback(Fx&& func) {
+            bound = [func](A... args) { std::invoke(func, args...); };
+            hash = reinterpret_cast<size_t>(this) ^ reinterpret_cast<size_t>(&bound) ^ (&typeid(callback<A...>))->hash_code();
+        }
+        
+        /// Binds the a class function attached to an instance of that class.
+        template<typename T, class Fx>
+        void bind_callback(T* obj, Fx&& func) {
+            bound = [obj, func](A... args) { std::invoke(func, obj, args...); };
+            hash = reinterpret_cast<size_t>(this) ^ reinterpret_cast<size_t>(&bound) ^ reinterpret_cast<size_t>(obj) ^ (&typeid(callback<A...>))->hash_code();
         }
 
-        // Creates and binds the function callback for a static/lambda/global function.
-        template<class _Fx, std::size_t... Is>
-        void create(_Fx&& func, std::integer_sequence<std::size_t, Is...>) {
-            this->func = std::function<void(A...)>(std::bind(func, placeholder_template<Is>()...));
-        }
     public:
-        /// Compares equality of callbacks.
-        inline bool operator == (const callback<A...>& cb) noexcept { return (hash == cb.hash); }
-        /// Compares not-equality of callbacks.
-        inline bool operator != (const callback<A...>& cb) noexcept { return (hash != cb.hash); }
-        /// Executes this callback with templated arguments.
-        inline callback<A...>& operator () (A... args) noexcept { func(args...); return (*this); }
-        /// Copy constructor.
-        inline callback<A...>& operator = (const callback<A...>& cb) { return clone(cb); }
-
-        /// Construct a callback with a template T reference for instance/member functions.
-        template<typename T, class _Fx>
-        callback(T* obj, _Fx&& func) {
-            hash = reinterpret_cast<size_t>(&this->func) ^ reinterpret_cast<size_t>(obj) ^ (&typeid(callback<A...>))->hash_code();
-            create(obj, func, std::make_integer_sequence<std::size_t, sizeof...(A)> {});
-        }
-
-        /// Construct a callback with template _Fx for static/lambda/global functions.
-        template<class _Fx>
-        callback(_Fx&& func) {
-            hash = reinterpret_cast<size_t>(&this->func) ^ (&typeid(callback<A...>))->hash_code();
-            create(func, std::make_integer_sequence<std::size_t, sizeof...(A)> {});
-        }
-
-        /// Executes this callback with arguments.
-        callback& invoke(A... args) { func(args...); return (*this); }
-
-        /// Returns this callbacks hash code.
+        /// Create a callback to a static or lambda function.
+        template<typename T, class Fx> callback(T* obj, Fx func) { bind_callback(obj, func); }
+        /// Create a callback to a class function attached to an instance of that class.
+        template<class Fx> callback(Fx func) { bind_callback(func); }
+        /// Compares the underlying hash_code of the callback function(s).
+        bool operator == (const callback<A...>& cb) { return hash == cb.hash; }
+        /// Inequality Compares the underlying hash_code of the callback function(s).
+        bool operator != (const callback<A...>& cb) { return hash != cb.hash; }
+        /// Invoke this callback with required arguments.
+        callback<A...>& invoke(A... args) { bound(args...); return (*this); }
+        /// Returns the unique hash code for this callback function.
         constexpr size_t hash_code() const throw() { return hash; }
-
-        /// Clones this callback function.
-        callback<A...>& clone(const callback<A...>& cb) {
-            func = cb.func;
-            hash = cb.hash;
-            return (*this);
-        }
     };
-    #pragma endregion
-    #pragma region Invokable Event
-    /// Thread-safe event handler for callbacks.
+
     template<typename... A>
     class invokable {
     protected:
-        /// Shared resource to manage multi-thread resource locking.
+        /// Resource lock for thread-safe accessibility.
         std::mutex safety_lock;
-        /// Vector list of function callbacks associated with this invokable event.
+        /// Record of stored callbacks to invoke.
         std::vector<callback<A...>> callbacks;
-    public:
-        /// Adds a callback to this event.
-        inline invokable<A...>& operator += (const callback<A...>& cb) noexcept { return hook(cb); }
-        /// Removes a callback from this event.
-        inline invokable<A...>& operator -= (const callback<A...>& cb) noexcept { return unhook(cb); }
-        /// Removes all registered callbacks and adds a new callback.
-        inline invokable<A...>& operator = (const callback<A...>& cb) noexcept { return rehook(cb); }
-        /// Execute all registered callbacks.
-        inline invokable<A...>& operator () (A... args) noexcept { return invoke(args...); }
-        // Copies all of the callbacks from the passed invokable object into this invokable object.
-        inline invokable<A...>& operator = (const invokable<A...>&& other) noexcept { return clone(other); }
 
+    public:
         /// Adds a callback to this event, operator +=
         invokable<A...>& hook(const callback<A...> cb) {
             std::lock_guard<std::mutex> g(safety_lock);
@@ -108,7 +65,7 @@
         /// Removes a callback from this event, operator -=
         invokable<A...>& unhook(const callback<A...> cb) {
             std::lock_guard<std::mutex> g(safety_lock);
-            std::erase_if(callbacks, [cb](callback<A...> i) { return cb == i; });
+            std::erase_if(callbacks, [cb](callback<A...> c){ return cb.hash_code() == c.hash_code(); });
             return (*this);
         }
 
@@ -130,15 +87,9 @@
         /// Execute all registered callbacks, operator ()
         invokable<A...>& invoke(A... args) {
             std::lock_guard<std::mutex> g(safety_lock);
-            for (callback<A...>& cb : callbacks) cb.invoke(args...);
-            return (*this);
-        }
-
-        /// Copies all of the callbacks from the passed invokable object into this invokable object.
-        invokable<A...>& clone(const invokable<A...>& invk) {
-            if (this != &invk) callbacks.assign(invk.callbacks.begin(), invk.callbacks.end());
+            for(callback<A...> cb : callbacks) cb.invoke(args...);
             return (*this);
         }
     };
-    #pragma endregion
+
 #endif
